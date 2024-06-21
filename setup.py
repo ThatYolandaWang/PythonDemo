db96870cf91dc59fb9768f72fe7c5d67218da4df
ThatYolandaWang <wangying123820@gmail.com>
2024-06-21 15:10:17 +0200

    main

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     # 指定项目名称，我们在后期打包时，这就是打包的包名称，当然打包时的名称可能还会包含下面的版本号哟~
-    name='pytest',
+    name='pythondemo',
     # 指定版本号
     version='0.1.1',
     # 这是对当前项目的一个描述
